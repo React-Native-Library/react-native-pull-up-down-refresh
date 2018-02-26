435cd28fa79f34cfad076f06f5629626b6df2a4b
Richard Jiang <richard.jiang@delawareconsulting.com>
2018-02-26 11:39:13 +0800

    upgrade 0.51

--- android/src/main/java/com/reactnativecomponent/swiperefreshlayout/RCTSwipeRefreshLayoutPackage.java
+++ android/src/main/java/com/reactnativecomponent/swiperefreshlayout/RCTSwipeRefreshLayoutPackage.java
@@ -20,7 +20,6 @@ public class RCTSwipeRefreshLayoutPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
